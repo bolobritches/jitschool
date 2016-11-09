7e3b8bd8c5ab2d73e52550d301c4a87e735c40af
Sean McGrew <SMcGrew@catapult.local>
2016-11-09 12:14:17 -0500

    20161109 db test

---
+++ testDB.sql
@@ -0,0 +1,13 @@
+CREATE TABLE player
+( player_id int NOT NULL,
+  lastname char(50) NOT NULL,
+  firstname char(50) NOT NULL,
+  nickname char(50),
+  birthyear int
+);
+
+/* CREATE TABLE ranks (rank_id label image)
+
+CREATE TABLE rankset (set_id rank_id set_sequence kid_flag)
+
+CREATE TABLE stripechoice (stripeset_id) */
